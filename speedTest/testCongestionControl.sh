e959872e794a6244373f7299f7fc88f6a8dceb57
root <root@vultr.guest>
2020-10-29 14:33:32 +0000

    create automatic scripts

---
+++ speedTest/testCongestionControl.sh
@@ -0,0 +1,62 @@
+IP=$1;
+CONGESTION=$2;
+FLOW=$3;
+
+if [[ "$CONGESTION" = "cubic" ]];
+then
+	if [[ $FLOW -gt 1 ]];
+	then
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+	else
+		iperf3 -c $IP -p 5201 -t 60 > $CONGESTION'1.txt'
+    fi
+elif [[ "$CONGESTION" = "reno" ]];
+then
+	if [[ $FLOW -gt 1 ]];
+	then
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+	else
+		iperf3 -c $IP -p 5201 -t 60 > $CONGESTION'1.txt'
+    fi
+elif [[ "$CONGESTION" = "bbr" ]];
+then
+	if [[ $FLOW -gt 1 ]];
+	then
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+	else
+		iperf3 -c $IP -p 5201 -t 60 > $CONGESTION'1.txt'
+    fi
+
+elif [[ "$CONGESTION" = "bbrplus" ]];
+then
+	if [[ $FLOW -gt 1 ]];
+	then
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+	else
+		iperf3 -c $IP -p 5201 -t 60 > $CONGESTION'1.txt'
+    fi
+elif [[ "$CONGESTION" = "tsunami" ]];
+then
+	if [[ $FLOW -gt 1 ]];
+	then
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+	else
+		iperf3 -c $IP -p 5201 -t 60 > $CONGESTION'1.txt'
+    fi
+elif [[ "$CONGESTION" = "nanqilang" ]];
+then
+	if [[ $FLOW -gt 1 ]];
+	then
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+	else
+		iperf3 -c $IP -p 5201 -t 60 > $CONGESTION'1.txt'
+    fi
+elif [[ "$CONGESTION" = "lotserver" ]];#iperf3无法在参数中匹配
+then
+	if [[ $FLOW -gt 1 ]];
+	then
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW > $CONGESTION$FLOW'.txt'
+	else
+		iperf3 -c $IP -p 5201 -t 60 > $CONGESTION'1.txt'
+    fi
+fi
