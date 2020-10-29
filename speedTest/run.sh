412b96ef7df074c23617657111f62c5d0a29ebb5
baolintian <tianbaolin1@gmail.com>
2020-10-30 00:33:49 +0800

    add run srcipt

---
+++ speedTest/run.sh
@@ -0,0 +1,61 @@
+./testCongestionControl.sh 144.202.103.115 reno 1 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 reno 8 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 reno 16 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 reno 32 JU
+
+
+./testCongestionControl.sh 144.202.103.115 cubic 1 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 cubic 8 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 cubic 16 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 cubic 32 JU
+
+
+./testCongestionControl.sh 144.202.103.115 bbr 1 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 bbr 8 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 bbr 16 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 bbr 32 JU
+
+
+./testCongestionControl.sh 144.202.103.115 bbrplus 1 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 bbrplus 8 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 bbrplus 16 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 bbrplus 32 JU
+
+
+./testCongestionControl.sh 144.202.103.115 tsunami 1 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 tsunami 8 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 tsunami 16 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 tsunami 32 JU
+
+
+./testCongestionControl.sh 144.202.103.115 nanqilang 1 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 nanqilang 8 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 nanqilang 16 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 nanqilang 32 JU
+
+
+./testCongestionControl.sh 144.202.103.115 lotserver 1 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 lotserver 8 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 lotserver 16 JU
+sleep 5;
+./testCongestionControl.sh 144.202.103.115 lotserver 32 JU
--- speedTest/testCongestionControl.sh
+++ speedTest/testCongestionControl.sh
@@ -1,62 +1,63 @@
 IP=$1;
 CONGESTION=$2;
 FLOW=$3;
+DIRECTION=$4;
 
 if [[ "$CONGESTION" = "cubic" ]];
 then
 	if [[ $FLOW -gt 1 ]];
 	then
-		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $DIRECTION$CONGESTION$FLOW'.txt'
 	else
-		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $CONGESTION'1.txt'
+		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $DIRECTION$CONGESTION'1.txt'
     fi
 elif [[ "$CONGESTION" = "reno" ]];
 then
 	if [[ $FLOW -gt 1 ]];
 	then
-		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $DIRECTION$CONGESTION$FLOW'.txt'
 	else
-		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $CONGESTION'1.txt'
+		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $DIRECTION$CONGESTION'1.txt'
     fi
 elif [[ "$CONGESTION" = "bbr" ]];
 then
 	if [[ $FLOW -gt 1 ]];
 	then
-		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $DIRECTION$CONGESTION$FLOW'.txt'
 	else
-		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $CONGESTION'1.txt'
+		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $DIRECTION$CONGESTION'1.txt'
     fi
 
 elif [[ "$CONGESTION" = "bbrplus" ]];
 then
 	if [[ $FLOW -gt 1 ]];
 	then
-		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $DIRECTION$CONGESTION$FLOW'.txt'
 	else
-		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $CONGESTION'1.txt'
+		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $DIRECTION$CONGESTION'1.txt'
     fi
 elif [[ "$CONGESTION" = "tsunami" ]];
 then
 	if [[ $FLOW -gt 1 ]];
 	then
-		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $DIRECTION$CONGESTION$FLOW'.txt'
 	else
-		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $CONGESTION'1.txt'
+		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $DIRECTION$CONGESTION'1.txt'
     fi
 elif [[ "$CONGESTION" = "nanqilang" ]];
 then
 	if [[ $FLOW -gt 1 ]];
 	then
-		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $CONGESTION > $CONGESTION$FLOW'.txt'
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW -C $DIRECTION$CONGESTION > $CONGESTION$FLOW'.txt'
 	else
-		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $CONGESTION'1.txt'
+		iperf3 -c $IP -p 5201 -t 60 -C $CONGESTION > $DIRECTION$CONGESTION'1.txt'
     fi
 elif [[ "$CONGESTION" = "lotserver" ]];#iperf3无法在参数中匹配
 then
 	if [[ $FLOW -gt 1 ]];
 	then
-		iperf3 -c $IP -p 5201 -t 60 -P $FLOW > $CONGESTION$FLOW'.txt'
+		iperf3 -c $IP -p 5201 -t 60 -P $FLOW > $DIRECTION$CONGESTION$FLOW'.txt'
 	else
-		iperf3 -c $IP -p 5201 -t 60 > $CONGESTION'1.txt'
+		iperf3 -c $IP -p 5201 -t 60 > $DIRECTION$CONGESTION'1.txt'
     fi
 fi
