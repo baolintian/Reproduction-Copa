67d7eb9d922a73c0d4bf0b12f1160704a9a40b1e
baolintian <tianbaolin1@gmail.com>
2020-11-01 22:57:06 +0800

    update level

--- multi-stream/sender.sh
+++ multi-stream/sender.sh
@@ -1,7 +1,6 @@
 IP=$1
 
-
-(bash bbr.sh $IP 1 > bbr1.txt &) ; (bash cubic.sh $IP 1 > cubic1.txt & ) ;  (bash reno.sh  1> reno.txt &)
+(bash bbr.sh $IP 1 > bbr1.txt &) ; (bash cubic.sh $IP 1 > cubic1.txt & ) ;  (bash reno.sh $IP 1 > reno.txt &)
 echo "here"
 sleep 40;
 (bash bbr.sh $IP 1 > bbr2.txt &) ; (bash cubic.sh $IP 8 > cubic2.txt & )
